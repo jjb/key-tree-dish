1cb844a4fb2a5abf70411f92b8862edb5eec2f23
John Bachir <j@jjb.cc>
2023-10-19 00:49:16 -0400

    better variable name

--- benchmark/runner.rb
+++ benchmark/runner.rb
@@ -4,24 +4,24 @@ require 'redis'
 require 'benchmark'
 require 'csv'
 
-def benchmark_setting(backend, n, testkeys, blob)
+def benchmark_setting(backend, number_of_items_in_cache, testkeys, blob)
   bm = Benchmark.measure{
-    n.times{
-      i = rand(n)
+    number_of_items_in_cache.times{
+      i = rand(number_of_items_in_cache)
       testkeys << i.to_s
       backend.set(i.to_s, blob)
     }
   }
-  bm.to_a[5]/n
+  bm.to_a[5]/number_of_items_in_cache
 end
 
-def benchmark_getting(backend, n, testkeys, blob)
+def benchmark_getting(backend, number_of_items_in_cache, testkeys, blob)
   bm = Benchmark.measure{
-    n.times{
-      backend.get(testkeys[rand(n)])
+    number_of_items_in_cache.times{
+      backend.get(testkeys[rand(number_of_items_in_cache)])
     }
   }
-  bm.to_a[5]/n
+  bm.to_a[5]/number_of_items_in_cache
 end
 
 set_csv = CSV.open("set_results.csv", "wb")
@@ -38,27 +38,29 @@ set_results = []
 get_results = []
 
 the_number = 5000
-(250..the_number).step(250).each do |n|
-  puts n
+(250..the_number).step(250).each do |number_of_items_in_cache|
+  puts number_of_items_in_cache
   ktd = KeyTreeDish.new
   r = Redis.new
 
   testkeys = []
 
   GC.start
-  ktd_set_rate = benchmark_setting(ktd, n, testkeys, blob)
+  ktd_set_rate = benchmark_setting(ktd, number_of_items_in_cache, testkeys, blob)
 
   GC.start
-  r_set_rate = benchmark_setting(r, n, testkeys, blob)
+  ktd_get_rate = benchmark_getting(ktd, number_of_items_in_cache, testkeys, blob)
+
+  testkeys = []
 
   GC.start
-  ktd_get_rate = benchmark_getting(ktd, n, testkeys, blob)
+  r_set_rate = benchmark_setting(r, number_of_items_in_cache, testkeys, blob)
 
   GC.start
-  r_get_rate = benchmark_getting(r, n, testkeys, blob)
+  r_get_rate = benchmark_getting(r, number_of_items_in_cache, testkeys, blob)
 
-  set_csv << [n, ktd_set_rate, r_set_rate]
-  get_csv << [n, ktd_get_rate, r_get_rate]
+  set_csv << [number_of_items_in_cache, ktd_set_rate, r_set_rate]
+  get_csv << [number_of_items_in_cache, ktd_get_rate, r_get_rate]
 end
 set_csv.close
 get_csv.close
